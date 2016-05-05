bdc0a47bc1f4ddee1e5326e8b5dc02dcb7398ef6
DXTimer <ibbakalov@gmail.com>
2016-05-05 18:00:47 +0100

    Added Rakefile

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'database_cleaner'
 gem 'sinatra'
+gem 'rake'
 
 group :development, :test do
 	gem 'capybara'
--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -6,4 +6,3 @@ require_relative 'link'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://postgres:admin@localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
