bbc9864758dff96c2eb9dd45e312f1411c15416a
Lexi Tran <hazukitran@gmail.com>
2016-05-06 19:18:39 +0700

    Step 19: Incomplete

--- spec/features/manage_user_spec.rb
+++ spec/features/manage_user_spec.rb
@@ -1,9 +1,13 @@
 feature 'Sign up' do
   
   scenario "User can sign up for an account" do
-    expect { sign_up }.to change(User, :count).by(1)
+    expect { sign_up_correct }.to change(User, :count).by(1)
     expect(page).to have_content "Welcome, hazukitran@gmail.com"
     expect(User.first.email).to eq 'hazukitran@gmail.com'
   end
-  
+
+  scenario 'checks mismatching password' do
+    expect { sign_up_incorrect }.not_to change(User, :count)
+  end
+
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,6 +1,15 @@
-def sign_up
+def sign_up_correct
   visit '/signup'
   fill_in :email, with: 'hazukitran@gmail.com'
   fill_in :password, with: 'hello'
+  fill_in :password_confirmation, with: 'hello'
+  click_button 'Sign up'
+end
+
+def sign_up_incorrect
+  visit '/sign_up'
+  fill_in :email, with: 'hazukitran@gmail.com'
+  fill_in :password, with: 'hello'
+  fill_in :password_confirmation, with: 'helloworld'
   click_button 'Sign up'
 end
